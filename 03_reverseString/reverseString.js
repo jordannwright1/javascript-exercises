71a1928f46e94ed31482a7080a45e142010da5ba
Jordan <jordannwright1@gmail.com>
2025-04-05 16:14:42 -0400

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  //for each letter starting from the end of the string//
+  //moving towards the beginning of the string add to newStr//
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    let letter = str[i];
+    newStr += letter;
+  }
+  return newStr;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
